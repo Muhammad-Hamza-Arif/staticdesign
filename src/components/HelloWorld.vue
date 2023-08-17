a0901816f9eb4131b3970899798e4608fb72a0dd
Muhammad Hamza <hamza5545613@gmail.com>
2023-08-17 15:02:53 +0500

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -422,59 +422,41 @@
     </div>
 
     <!-- last-section -->
-    <section id="newsletter" aria-label="Newsletter" class="mt-20">
+    <section class="mt-20">
       <div class="mx-auto max-w-7xl px-8">
         <div
-          class="relative -mx-4 overflow-hidden bg-indigo-50 px-4 py-20  sm:px-6 md:mx-0 rounded-5xl md:px-16 xl:px-24 xl:py-36"
+          class=" bg-indigo-50  rounded-5xl px-16 py-36"
         >
-          
           <div
-            class="relative mx-auto grid max-w-2xl grid-cols-1 gap-x-32 gap-y-14 xl:max-w-none xl:grid-cols-2"
+            class=" mx-auto grid  gap-x-32 gap-y-14 max-w-none grid-cols-2"
           >
             <div>
               <p
-                class="font-display text-4xl font-medium tracking-tighter text-blue-900 sm:text-5xl"
+                class=" text-4xl font-medium text-blue-900"
               >
                 Stay up to date
               </p>
-              <p class="mt-4 text-lg tracking-tight text-blue-900">
+              <p class="mt-4 text-lg text-blue-900">
                 Get updates on all of our events and be the first to get
                 notified when tickets go on sale.
               </p>
             </div>
             <form>
-              <h3 class="text-lg font-semibold tracking-tight text-blue-900">
+              <h3 class="text-lg font-semibold text-blue-900">
                 Sign up to our newsletter
               </h3>
               <div
-                class="mt-5 flex rounded-3xl bg-white py-2.5 pr-2.5 shadow-xl shadow-blue-900/5 focus-within:ring-2 focus-within:ring-blue-900"
+                class="mt-5 flex rounded-3xl bg-white py-2.5 pr-2.5 shadow-xl shadow-blue-900/5"
               >
                 <input
                   type="email"
-                  required=""
                   placeholder="Email address"
-                  aria-label="Email address"
-                  class="-my-2.5 flex-auto bg-transparent pl-6 pr-2.5 text-base text-slate-900 placeholder:text-slate-400 focus:outline-none"
+                  class="-my-2.5 flex-auto bg-transparent pl-6 pr-2.5 text-slate-900 placeholder:text-slate-400"
                 /><button
-                  class="inline-flex justify-center rounded-2xl bg-blue-600 p-4 text-base font-semibold text-white hover:bg-blue-500 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:text-white/70"
+                  class="rounded-2xl bg-blue-600 p-4 font-semibold text-white hover:bg-blue-500"
                   type="submit"
                 >
-                  <span class="sr-only sm:not-sr-only">Sign up today</span
-                  ><span class="sm:hidden"
-                    ><svg
-                      aria-hidden="true"
-                      viewBox="0 0 24 24"
-                      class="h-6 w-6"
-                    >
-                      <path
-                        d="m14 7 5 5-5 5M19 12H5"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      ></path></svg>
-                    </span>
+                  Sign up today
                 </button>
               </div>
             </form>
@@ -484,10 +466,8 @@
     </section>
 
     <!-- footer -->
-    <footer class="flex-none py-16">
-      <div
-        class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-between md:flex-row"
-      >
+    <footer class="mt-20">
+      <div class="mx-auto max-w-7xl px-8 flex items-center justify-between">
         <svg
           aria-hidden="true"
           viewBox="0 0 183 48"
